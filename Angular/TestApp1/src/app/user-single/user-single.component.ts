41b55d56555dea2cd805ca4e5d3aa840db35bbaf
Riccardo Mammina <riccardo.mammina@gmail.com>
2021-10-27 14:26:37 +0200

    Aggiunto esercizio Angular 4

--- Angular/TestApp1/src/app/user-single/user-single.component.ts
+++ Angular/TestApp1/src/app/user-single/user-single.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ObjectInterface } from '../models/ObjectInterface';
 
 @Component({
   selector: 'app-user-single',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-single.component.css']
 })
 export class UserSingleComponent implements OnInit {
-
+  @Input() user!: ObjectInterface;
+  @Input() index!: number;
   constructor() { }
 
   ngOnInit(): void {
